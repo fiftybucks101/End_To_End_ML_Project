ed1da8817f2062b11630b5e5baf570a5c72b7f54
Fifty Bucks <fiftybucks001@gmail.com>
2024-06-05 15:50:17 +0545

    Data Transformation Done

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-import src.logger
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -22,5 +21,5 @@ if __name__=="__main__":
     try:
         a=1/0
     except Exception as e:
-        logging.info("Divide by Zero")
+        logging.info("Logging Checking with injestion")
         raise CustomException(e,sys)
