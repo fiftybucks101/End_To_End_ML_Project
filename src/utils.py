abb6cd3c1aad6cf97333ce6daea8f7a6d6e91523
Fifty Bucks <fiftybucks001@gmail.com>
2024-06-07 11:16:50 +0545

    Model Training

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,9 @@ import numpy as np
 import pandas as pd
 import dill 
 import pickle
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -19,3 +20,54 @@ def save_object(file_path,obj):
             
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
+    report = {}
+    try:
+    
+        for i, (model_name,model) in enumerate(models.items()):
+            param_grid = params[model_name]
+
+            gsv = GridSearchCV(estimator=model,param_grid=param_grid,n_jobs=-1)
+            gsv.fit(X_train,y_train)
+            model.set_params(**gsv.best_params_)
+            model.fit(X_train,y_train) 
+            y_pred = model.predict(X_test)
+            r2 = r2_score(y_test,y_pred)
+            report[model_name] = r2
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+# def evaluate_models(X_train, y_train,X_test,y_test,models,params):
+#     try:
+#         report = {}
+
+#         for i in range(len(list(models))):
+#             model = list(models.values())[i]
+#             para=params[list(models.keys())[i]]
+
+#             gs = GridSearchCV(model,para,cv=3)
+#             gs.fit(X_train,y_train)
+
+#             model.set_params(**gs.best_params_)
+#             model.fit(X_train,y_train)
+
+#             #model.fit(X_train, y_train)  # Train model
+
+#             y_train_pred = model.predict(X_train)
+
+#             y_test_pred = model.predict(X_test)
+
+#             train_model_score = r2_score(y_train, y_train_pred)
+
+#             test_model_score = r2_score(y_test, y_test_pred)
+
+#             report[list(models.keys())[i]] = test_model_score
+
+#         return report
+
+#     except Exception as e:
+#         raise CustomException(e, sys)
